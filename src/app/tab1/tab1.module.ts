5d773263197d84bd995b7edf3f89308e1f5bf29f
unknown <pedro.nieto.sanchez@gmail.com>
2019-05-25 03:20:26 +0100

    primer chart Line

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -5,12 +5,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 
+import { ChartsModule } from 'ng2-charts';
+
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
+    RouterModule.forChild([{ path: '', component: Tab1Page }]),
+    ChartsModule
   ],
   declarations: [Tab1Page]
 })
