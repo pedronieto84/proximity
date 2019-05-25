d88791ed7d5914e14e61df07cc849bc89fb2a3a3
unknown <pedro.nieto.sanchez@gmail.com>
2019-05-25 03:59:59 +0100

    tablas pero sin cargar datos

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab2Page }])
+    RouterModule.forChild([{ path: '', component: Tab2Page }]),
+    NgxDatatableModule
   ],
   declarations: [Tab2Page]
 })
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +7,83 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  
+
+  @ViewChild('mydatatable') mydatatable: any;
+
+  count: number = 50;
+  rows: any[] = [];
+  active: boolean = true;
+  temp: any[] = [];
+  cols: any = [
+    'name', 'gender', 'company'
+  ];
+
+  constructor(private cd: ChangeDetectorRef) {
+    this.fetch((data) => {
+      this.rows = data.map(d => {
+        d.updated = Date.now().toString();
+        return d;
+      });
+
+      this.temp = [...this.rows];
+    });
+
+    this.start();
+  }
+
+  randomNum(start: number, end: number): number {
+    return Math.floor(Math.random() * end) + start;
+  }
+
+  start(): void {
+    if(!this.active) return;
+
+    setTimeout(this.updateRandom.bind(this), 50);
+  }
+
+  stop(): void {
+    this.active = false;
+  }
+
+  add() {
+    this.rows.splice(0, 0, this.temp[this.count++]);
+  }
+
+  remove() {
+    this.rows.splice(0, this.rows.length);
+  }
+
+  updateRandom() {
+    const rowNum = this.randomNum(0, 5);
+    const cellNum = this.randomNum(0, 3);
+    const newRow = this.randomNum(0, 100);
+    const prop = this.cols[cellNum];
+    const rows = this.rows;
+
+    if(rows.length) {
+      const row = rows[rowNum];
+      row[prop] = rows[newRow][prop];
+      row.updated = Date.now().toString();
+    }
+
+    this.rows = [...this.rows];
+
+    // this.cd.markForCheck();
+    // this.mydatatable.update();
+    this.start();
+  }
+
+  fetch(cb: any): void {
+    const req = new XMLHttpRequest();
+    req.open('GET', `assets/data/company.json`);
+
+    req.onload = () => {
+      cb(JSON.parse(req.response));
+    };
+
+    req.send();
+  }
+
 
 }
